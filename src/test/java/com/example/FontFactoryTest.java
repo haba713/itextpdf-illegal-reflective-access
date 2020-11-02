3e21e89cab21a5d47a0dc73d7c26b0284d3489bb
Harri T <haba713@gmail.com>
2020-11-02 15:36:39 +0200

    https://stackoverflow.com/a/64644482/2158271

--- src/test/java/com/example/FontFactoryTest.java
+++ src/test/java/com/example/FontFactoryTest.java
@@ -3,12 +3,21 @@ package com.example;
 import org.junit.Test;
 
 import com.itextpdf.text.FontFactory;
+import org.junit.BeforeClass;
 
 public class FontFactoryTest {
 	
-	@Test
-	public void test() {
-		FontFactory.registerDirectories();
-	}
+    @BeforeClass
+    public static void prepare() {
+        Module mod = FontFactory.class.getClassLoader().getUnnamedModule();
+        if(mod == FontFactory.class.getModule()) {
+            Object.class.getModule().addOpens("java.nio", mod);
+        }
+    }
 
+    @Test
+    public void test() {
+            FontFactory.registerDirectories();
+	}
+	
 }
